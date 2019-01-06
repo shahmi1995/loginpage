4d263db1302e346ebc849a497f009b57c3e1c804
mohamad shahmi <amieparkson@gmail.com>
2019-01-06 09:42:55 +0800

    Add project files.

---
+++ loginpage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(loginpage.Startup))]
+namespace loginpage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
